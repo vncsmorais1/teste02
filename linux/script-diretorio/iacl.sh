3531caa45b809dc71585a3c9b54324f75526c2d8
vncsmorais1 <mavis2vini@gmail.com>
2023-04-03 10:32:50 -0300

    1.1.1.2

---
+++ linux/script-diretorio/iacl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo " Criando grupos de usuários ############"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando Usuários.... ##########"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_ADM
+
+useradd debora  -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwp -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissoes dos diretórios ... ##########"
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven 
+chown root:GRO_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "finalizando...###"
+
+
